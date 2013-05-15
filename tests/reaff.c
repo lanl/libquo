b3319b1b0443850de7eec9256a9871b996f5741f
Samuel K. Gutierrez <samuel@lanl.gov>
2013-05-15 11:13:31 -0600

    write rebind test... looks like this works on linux.

--- tests/reaff.c
+++ tests/reaff.c
@@ -21,32 +21,66 @@
 
 #include <hwloc.h>
 
+/* a test to see if we can rebind a running process */
+
+static void
+emit_binding(const hwloc_topology_t *t)
+{
+    char *str = NULL;
+    hwloc_cpuset_t cpu_set = hwloc_bitmap_alloc();
+
+    hwloc_get_cpubind(*t, cpu_set, HWLOC_CPUBIND_PROCESS);
+    hwloc_bitmap_asprintf(&str, cpu_set);
+    printf("%d's cpubind bitmap is: %s\n", (int)getpid(), str);
+
+    free(str);
+    hwloc_bitmap_free(cpu_set);
+}
+
 int
 main(int argc, char **argv)
 {
     int erc = EXIT_SUCCESS;
-    char *str = NULL;
-    hwloc_cpuset_t cpu_set = hwloc_bitmap_alloc();
+    unsigned ncores = 0;
+    hwloc_cpuset_t cpu_set = hwloc_bitmap_alloc(), first_bind = hwloc_bitmap_alloc();
     hwloc_topology_t topology;
+    hwloc_obj_t first_core, last_core;
 
     /* allocate and initialize topology object. */
     hwloc_topology_init(&topology);
     /* build the topology */
     hwloc_topology_load(topology);
+    /* get some info */
+    ncores = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_CORE);
+    first_core = hwloc_get_obj_by_type(topology, HWLOC_OBJ_CORE, 0);
+    last_core = hwloc_get_obj_by_type(topology, HWLOC_OBJ_CORE, ncores - 1);
+    /* stash current binding */
+    hwloc_get_cpubind(topology, first_bind, HWLOC_CPUBIND_PROCESS);
+    /* start the madness */
+    printf("starting rebinding test on pid %d\n", getpid());
 
-    printf("starting reaffinitization test on pid %d\n", (int)getpid());
+    emit_binding(&topology);
 
-    hwloc_get_cpubind(topology, cpu_set, HWLOC_CPUBIND_PROCESS);
+    printf("changing binding...\n");
 
-    hwloc_bitmap_asprintf(&str, cpu_set);
+    hwloc_bitmap_copy(cpu_set, last_core->cpuset);
+    hwloc_bitmap_singlify(cpu_set);
+    hwloc_set_cpubind(topology, cpu_set, HWLOC_CPUBIND_PROCESS);
 
-    printf("TEST: %s\n", str);
+    emit_binding(&topology);
 
-    printf("done with reaffinitization test\n");
+    printf("reverting binding...\n");
 
+    hwloc_set_cpubind(topology, first_bind, HWLOC_CPUBIND_PROCESS);
 
+    emit_binding(&topology);
+
+    printf("done with rebinding test\n");
+
+    /* cleanup */
     hwloc_bitmap_free(cpu_set);
-    free(str);
+    hwloc_bitmap_free(first_bind);
+    hwloc_topology_destroy(topology);
 
     return erc;
 }
