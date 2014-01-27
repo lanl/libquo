d0169c99c2b20ce58429e799d86da6b48815bc26
Samuel K. Gutierrez <samuel@lanl.gov>
2014-01-27 11:41:45 -0700

    Fix YCM includes.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -54,10 +54,9 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c',
-'-I',
-'.',
-'-I',
-'./src'
+'-I.',
+'-I./src',
+'-I./src/hwloc/include'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
