87f957f51b00ad1824414661c206548d773035fc
Douglas Otstott <Douglas.Otstott@gmail.com>
2015-10-20 17:46:04 -0600

    Added openmp demo to automake path

--- demos/openmp/sample.c
+++ demos/openmp/sample.c
@@ -3,7 +3,7 @@
 #include "mpi.h"
 #include "quo.h"
 #include <omp.h>
-#include <sched.h>
+ <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -61,5 +61,5 @@ err:
         QUO_free(context);
 
     MPI_Finalize();
-    return 1;
+    return 0;
 }
