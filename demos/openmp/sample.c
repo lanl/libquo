de6a9e67337ab7f245373e688a4650144d646ee4
Douglas Otstott <Douglas.Otstott@gmail.com>
2015-10-20 17:40:03 -0600

    Added openmp demos to the automake path

--- demos/openmp/sample.c
+++ demos/openmp/sample.c
@@ -2,73 +2,64 @@
 
 #include "mpi.h"
 #include "quo.h"
+#include <omp.h>
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
 QUO_context context;
 int rank, size;
 
-/* void pin_threads() { */
-/*   int thread_num; */
-/*   cpu_set_t set; */
-  
-
-/*   get_cur_bind((context->hwloc, getpid(), &set); */
-  
-/*   print("count: %d\n", CPU_COUNT(&set)); */
-
-/*   return; */
-  
-/*   CPU_ZERO(&set); */
-  
-/*   thread_num = omp_get_thread_num(); */
-/*   CPU_SET(thread_num, &set); */
-/*   sched_setaffinity(syscall(SYS_gettid), sizeof(cpu_set_t), &set);   */
-/* } */
-
 void toString(char *text){
-  printf("%s: Rank %d, thread: %d, PID: %d, CPU: %u\n", text, rank, omp_get_thread_num(), getpid(), sched_getcpu());
-  fflush(stdout);
+    printf("%s: Rank %d, thread: %d, PID: %d, CPU: %u\n",
+           text, rank, omp_get_thread_num(), getpid(), sched_getcpu());
+    fflush(stdout);
 }
 
-void main() {
-  int number, i;
-  
-  MPI_Init(NULL, NULL);
-  
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+int main() {
+    int number, i;
 
-  QUO_create(&context);
+    if (MPI_SUCCESS != MPI_Init(NULL, NULL)) return 1;
 
-  QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, 0);
-  omp_set_nested(1);
-    /* if (rank > 0)  */
-    /*   MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL); */
-    
-#pragma omp parallel num_threads(4)
-    {  
-      QUO_bind_threads(context, QUO_OBJ_SOCKET, 0);
-      /* toString("1 configuration"); */
-      /* sleep(1); */
-      /* printf("\n"); */
-      /* sleep(1); */
-      
-/* #pragma omp parallel num_threads(2) */
-/*       { */
-/* 	QUO_bind_threads(context, QUO_OBJ_SOCKET, rank%2); */
-/* 	/\* toString("2 configuration"); *\/ */
-/*       } */
+    if (MPI_SUCCESS != MPI_Comm_rank(MPI_COMM_WORLD, &rank)) goto err;
+
+    if (MPI_SUCCESS !=  MPI_Comm_size(MPI_COMM_WORLD, &size)) goto err;
+
+    if (QUO_SUCCESS != QUO_create(&context)) goto err;
+
+    if (QUO_SUCCESS !=
+        QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank%2))
+    {
+        printf("QUO_bind failed\n");
+        goto err;
     }
-    
-    /* if(rank < 4) */
-    /*   MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD); */
 
-    QUO_free(context);
+    omp_set_nested(1);
+
+#pragma omp parallel num_threads(4)
+    {
+        if (QUO_SUCCESS != QUO_bind_threads(context, QUO_OBJ_SOCKET, rank%2))
+            printf("QUO_bind_threads failed\n");
+
+        toString("First configuration");
+
+#pragma omp parallel num_threads(2)
+        {
+            if (QUO_SUCCESS != QUO_bind_threads(context,
+                                                QUO_OBJ_SOCKET, rank%2))
+                printf("QUO_bind_threads failed\n");
+
+            toString("Second configuration");
+        }
+    }
+
+err:
+    if(context)
+        QUO_free(context);
 
     MPI_Finalize();
+    return 1;
 }
