ef7e42b8308344a3aabc1465a720a6bdee4b9960
Douglas Otstott <Douglas.Otstott@gmail.com>
2015-10-20 16:41:23 -0600

    Cleaned up openmp sample programs

--- demos/openmp/sample.c
+++ demos/openmp/sample.c
@@ -1,19 +1,41 @@
+#define _GNU_SOURCE
+
 #include "mpi.h"
 #include "quo.h"
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
 
 QUO_context context;
 int rank, size;
 
+/* void pin_threads() { */
+/*   int thread_num; */
+/*   cpu_set_t set; */
+  
+
+/*   get_cur_bind((context->hwloc, getpid(), &set); */
+  
+/*   print("count: %d\n", CPU_COUNT(&set)); */
+
+/*   return; */
+  
+/*   CPU_ZERO(&set); */
+  
+/*   thread_num = omp_get_thread_num(); */
+/*   CPU_SET(thread_num, &set); */
+/*   sched_setaffinity(syscall(SYS_gettid), sizeof(cpu_set_t), &set);   */
+/* } */
+
 void toString(char *text){
-  printf("%s: Rank %d, CPU: %u\n", text, rank, sched_getcpu());
+  printf("%s: Rank %d, thread: %d, PID: %d, CPU: %u\n", text, rank, omp_get_thread_num(), getpid(), sched_getcpu());
   fflush(stdout);
 }
 
 void main() {
-  int number;
+  int number, i;
   
   MPI_Init(NULL, NULL);
   
@@ -23,188 +45,30 @@ void main() {
 
   QUO_create(&context);
 
-  if(rank < 4){
-    QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank%2);
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-    omp_set_num_threads(6);
-#pragma omp parallel proc_bind(close) 
-    {
-      toString("First configuration");
+  QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, 0);
+  omp_set_nested(1);
+    /* if (rank > 0)  */
+    /*   MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL); */
+    
+#pragma omp parallel num_threads(4)
+    {  
+      QUO_bind_threads(context, QUO_OBJ_SOCKET, 0);
+      /* toString("1 configuration"); */
+      /* sleep(1); */
+      /* printf("\n"); */
+      /* sleep(1); */
+      
+/* #pragma omp parallel num_threads(2) */
+/*       { */
+/* 	QUO_bind_threads(context, QUO_OBJ_SOCKET, rank%2); */
+/* 	/\* toString("2 configuration"); *\/ */
+/*       } */
     }
-    if(rank < 3)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-  
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
+    
+    /* if(rank < 4) */
+    /*   MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD); */
 
-  /*Set up second configuration: 1 rank per socket, 10 Threads per
-    rank*/
-  if(rank > 1)  
-    QUO_barrier(context);
-  else{ 
-    QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank);
-    if (rank==1) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(10) proc_bind(close)
-    {
-      toString("Second configuration");
-    }
-    if (rank==0)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-     
-    QUO_bind_pop(context);  
-    QUO_barrier(context);
-  }
-  
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
+    QUO_free(context);
 
-  /*Return to first configuration: 2 ranks per socket, 5 Threads per
-    rank*/
-  if(rank < 4){
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(5) proc_bind(close) 
-    {
-      toString("First Configuration(2)");
-    }
-    if(rank < 3)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-  
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  /*Set up third configuration: 5 ranks per socket, 2 Threads per
-    rank*/
-  if(rank < 10) {
-    QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank%2);
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(2) proc_bind(close) 
-    {
-      toString("Third Configuration");
-    }
-    if(rank < 9)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_bind_pop(context);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  /*Return to first configuration: 2 ranks per socket, 5 Threads per
-    rank*/   
-  if(rank < 4){
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(5) proc_bind(close) 
-    {
-      toString("First Configuration(3)");
-    }
-    if(rank < 3)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
- /*Set up second configuration: 1 rank per socket, 10 Threads per
-    rank*/
-  if(rank > 1)  
-    QUO_barrier(context);
-  else{ 
-    QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank);
-    if (rank==1) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(10) proc_bind(close)
-    {
-      toString("Second Configuration(2)");
-    }
-    if (rank==0)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-     
-    QUO_bind_pop(context);  
-    QUO_barrier(context);
-  }
-  
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  /*Return to first configuration: 2 ranks per socket, 5 Threads per
-    rank*/     
-  if(rank < 4){
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(5) proc_bind(close) 
-    {
-      printf("First Configuration(4): Rank %d, CPU: %u\n", rank, sched_getcpu());
-      fflush(stdout);
-    }
-    if(rank < 3)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-  
-  if(rank==0) {
-    printf("\n");
-    fflush(stdout);
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  /*Set up third configuration: 5 ranks per socket, 2 Threads per
-    rank*/
-  if(rank < 10) {
-    QUO_bind_push(context, QUO_BIND_PUSH_PROVIDED, QUO_OBJ_SOCKET, rank%2);
-    if (rank > 0) 
-      MPI_Recv(&number, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, NULL);
-#pragma omp parallel num_threads(2) proc_bind(close) 
-    {
-      toString("Third Configuration(2)");
-    }
-    if(rank < 9)
-      MPI_Send(&number, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
-    QUO_bind_pop(context);
-    QUO_barrier(context);
-  }
-  else
-    QUO_barrier(context);
-  
-  QUO_free(context);
-
-  MPI_Finalize();
+    MPI_Finalize();
 }
