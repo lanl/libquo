4b04e74cc7313551049829fdad5b307536e06c19
Samuel K. Gutierrez <samuel@lanl.gov>
2017-01-24 15:54:02 -0700

    Start of proper QUO demo with data movement.

---
+++ demos/mpi-openmp/dgemv.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the libquo project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
