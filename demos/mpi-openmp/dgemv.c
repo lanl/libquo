788e67867f945c579f85fb150a37e0d6421b8291
Samuel K. Gutierrez <samuel@lanl.gov>
2017-01-26 14:55:20 -0700

    Add problem init code.

--- demos/mpi-openmp/dgemv.c
+++ demos/mpi-openmp/dgemv.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <inttypes.h>
 
 #include "mpi.h"
 #include "omp.h"
@@ -21,6 +24,10 @@ typedef struct dgemv_t {
     int pe;
     /* A QUO contex. */
     QUO_context qc;
+    /* Local number of rows. */
+    int64_t m;
+    /* Local number of columns. */
+    int64_t n;
 } dgemv_t;
 
 enum {
@@ -75,6 +82,56 @@ free_quo_context(dgemv_t *d)
     return SUCCESS;
 }
 
+static bool
+starts_with(char *s,
+            char *prefix)
+{
+    size_t n = strlen( prefix );
+    if (strncmp(s, prefix, n)) return false;
+    return true;
+}
+
+static int
+init_dgem(dgemv_t *d,
+          int argc,
+          char **argv)
+{
+    static const int n_params = 2;
+    static const int64_t defaultv = 4096;
+    int64_t iparams[] = {defaultv, defaultv};
+    /* Local sizes. */
+    static char *params[16] = {
+        "--m=",
+        "--n="
+    };
+
+    /* Parse argv. */
+    for (int i = 1; i < argc; ++i) {
+        for (int pi = 0; pi < n_params; ++pi) {
+            if (starts_with(argv[i], params[pi])) {
+                if (sscanf(argv[i] + strlen(params[pi]),
+                           "%" PRId64, iparams + pi) != 1 || iparams[pi] < 1) {
+                    iparams[pi] = defaultv;
+                }
+            }
+        }
+    }
+
+    d->m = iparams[0];
+    d->n = iparams[1];
+
+    pprintf(0 == d->pe,
+            "# Local Matrix:\n"
+            "# m=%" PRId64 "\n"
+            "# n=%" PRId64 "\n"
+            "# Global Matrix:\n"
+            "# m=%" PRId64 "\n"
+            "# n=%" PRId64 "\n",
+            d->m, d->n, d->m * d->numpe, d->n);
+
+    return SUCCESS;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -83,7 +140,9 @@ main(int argc, char **argv)
 
     /* Init MPI library. */
     if (SUCCESS != (rc = init_mpi(&dgem, argc, argv))) goto out;
-    /* Now we can create a QUO context. */
+    /* Problem init. */
+    if (SUCCESS != (rc = init_dgem(&dgem, argc, argv))) goto out;
+    /* Create a QUO context (can be done anytime after MPI_Init). */
     if (SUCCESS != (rc = create_quo_context(&dgem))) goto out;
 
     if (SUCCESS != (rc = free_quo_context(&dgem))) goto out;
