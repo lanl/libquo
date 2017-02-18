6c130bba3ea674ec2375e0f1f95d4071e2ca2a3d
Samuel K. Gutierrez <samuel@lanl.gov>
2017-02-17 21:33:47 -0700

    Serial zeta done.

--- demos/mpi-openmp/zeta.c
+++ demos/mpi-openmp/zeta.c
@@ -6,8 +6,29 @@
  * top-level directory of this distribution.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <math.h>
+
+static double
+zeta_local(double s,
+           int64_t low,
+           int64_t high)
+{
+    double result = 0.0;
+
+    for (int64_t i = low; i < high; ++i) {
+        result += 1.0 / powl(i, s);
+    }
+
+    return result;
+}
+
 int
 main(int argc, char **argv)
 {
-    return 0;
+    double z2 = zeta_local(2, 1, 1000000);
+    printf("zeta(2)=%lf\n", z2);
+    return EXIT_SUCCESS;
 }
