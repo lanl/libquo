ce9561718a68d291d7af276e953573c956daaa1f
Samuel K. Gutierrez <samuel@lanl.gov>
2017-02-18 18:43:53 -0700

    Mostly done with parallel zeta(s) "library".

--- demos/mpi-openmp/zeta.c
+++ demos/mpi-openmp/zeta.c
@@ -6,29 +6,104 @@
  * top-level directory of this distribution.
  */
 
+/*
+ * Parallel library that calculates the Riemann zeta function for a given s.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <inttypes.h>
 #include <math.h>
 
-static double
-zeta_local(double s,
-           int64_t low,
-           int64_t high)
-{
-    double result = 0.0;
+#include "mpi.h"
 
-    for (int64_t i = low; i < high; ++i) {
-        result += 1.0 / powl(i, s);
+enum {
+    ZETA_SUCCESS = 0,
+    ZETA_FAILURE,
+    ZETA_INVALID_ARG,
+    ZETA_OOR
+};
+
+static int
+zetai(double s,
+      int64_t low,
+      int64_t high,
+      double *restrict z)
+{
+    for (int64_t zi = 0, i = low; i < high; ++i, ++zi) {
+        z[zi] = 1.0 / powl(i, s);
     }
 
+    return ZETA_SUCCESS;
+}
+
+static double
+sigma_zeta(int64_t n,
+           double *restrict z)
+{
+    double result = 0.0;
+    for (int64_t i = 0; i < n; ++i) {
+        result += z[i];
+    }
     return result;
+}
+
+int
+zeta(double s,
+     int64_t n,
+     double **z,
+     MPI_Comm comm,
+     double *result)
+{
+    if (s <= 1.0) return ZETA_INVALID_ARG;
+
+    int cid = 0, csize = 0;
+    if (MPI_SUCCESS != MPI_Comm_rank(comm, &cid))   return ZETA_FAILURE;
+    if (MPI_SUCCESS != MPI_Comm_size(comm, &csize)) return ZETA_FAILURE;
+
+    /* My bounds. */
+    int64_t low  = 1 + (cid * n);
+    int64_t high = (cid + 1) * n;
+    /* My array of zeta(i). */
+    double *ztmp = calloc(n, sizeof(ztmp));
+    if (!ztmp) return ZETA_OOR;
+    /* Fill in local z(i)s. */
+    int rc = zetai(s, low, high, ztmp);
+    if (ZETA_SUCCESS != rc) return rc;
+    /* Calculate local sum */
+    double partial_sum = sigma_zeta(n, ztmp);
+    /* Calculate global sum */
+    if (MPI_SUCCESS != MPI_Reduce(&partial_sum, result,
+                                  1, MPI_DOUBLE, MPI_SUM,
+                                  0, comm)) {
+        rc = ZETA_FAILURE;
+        goto out;
+    }
+out:
+    if (ZETA_SUCCESS != rc) free(ztmp);
+    else *z = ztmp;
+    return rc;
 }
 
 int
 main(int argc, char **argv)
 {
-    double z2 = zeta_local(2, 1, 1000000);
-    printf("zeta(2)=%lf\n", z2);
-    return EXIT_SUCCESS;
+    if (MPI_SUCCESS != MPI_Init(&argc, &argv)) return EXIT_FAILURE;
+    int cid = 0;
+    if (MPI_SUCCESS != MPI_Comm_rank(MPI_COMM_WORLD, &cid)) return EXIT_FAILURE;
+
+    double *z = NULL;
+    double res = 0.0;
+    int64_t n = 1024;
+    double s = 2.0;
+    int zrc = zeta(s, n, &z, MPI_COMM_WORLD, &res);
+    if (ZETA_SUCCESS != zrc) goto out;
+
+    const bool emit = (0 == cid);
+    if (emit) printf("z(%lf) = %lf\n", s, res);
+
+out:
+    MPI_Finalize();
+    return (ZETA_SUCCESS == zrc ? EXIT_SUCCESS : EXIT_FAILURE);
 }
