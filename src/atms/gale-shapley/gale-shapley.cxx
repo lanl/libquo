b4552e389416c1d49320ba416d3dc089ae5e27c1
Samuel K. Gutierrez <samuel@lanl.gov>
2017-03-27 13:33:19 -0600

    Start of atms support.

---
+++ src/atms/gale-shapley/gale-shapley.cxx
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the libquo project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * @file gale-shapley.cxx
+ */
+
+int
+gale_shapley(
+) {
+    return 0;
+}
