a969a535a74833eb351d6cc306487967d79d8294
Samuel K. Gutierrez <samuel@lanl.gov>
2017-03-28 15:49:08 -0600

    Checkpoint atms work.

---
+++ src/atms/quo-atms-init.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the libquo project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * @file quo_atms_init.c
+ */
+
+int
+QUO_atms_init(void)
+{
+    return 0;
+}
---
+++ src/atms/quo-atms.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the libquo project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * @file quo_atms.h
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int
+QUO_atms_init(void);
+
+#ifdef __cplusplus
+}
+#endif
