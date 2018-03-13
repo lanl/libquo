3a1414769353119d9f7b416313f0e8e6dbc7d0a1
Samuel K. Gutierrez <samuel@lanl.gov>
2018-03-13 16:07:12 -0600

    Checkpoint

--- demos/nice-tid-distrib.c
+++ demos/nice-tid-distrib.c
@@ -7,9 +7,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <assert.h>
 
 #include "mpi.h"
@@ -34,17 +31,6 @@ typedef struct inf_t {
     /* Comm used for communication between qid zeros. */
     MPI_Comm machine_delegate_comm;
 } inf_t;
-
-/**
- * rudimentary "pretty print" routine. not needed in real life...
- */
-static inline void
-demo_emit_sync(const inf_t *c)
-{
-    MPI_Barrier(c->init_comm);
-    MPI_Barrier(c->init_comm);
-    usleep((c->rank) * 10000);
-}
 
 /*
  * This is where all the machine naming, communicator setup and all that jazz
@@ -72,6 +58,10 @@ machine_info_setup(inf_t *inf)
     /* Name the nodes: 0 to n-1 */
     if (MPI_COMM_NULL != inf->machine_delegate_comm) {
         MPI_Comm_rank(inf->machine_delegate_comm, &inf->machine_id);
+        /* Make sure this is sane. */
+        int sanity = -1;
+        MPI_Comm_size(inf->machine_delegate_comm, &sanity);
+        assert(sanity == inf->n_machines);
     }
     /* Share the name with everyone on a node. */
     MPI_Bcast(&inf->machine_id, 1, MPI_INT, 0, inf->machine_comm);
@@ -80,10 +70,34 @@ machine_info_setup(inf_t *inf)
 static void
 tid_setup(inf_t *inf,
           QUO_obj_type_t target_res,
+          int max_local_res,
           int max_global_res,
           int **out_tids,
           int *out_tids_len)
 {
+    // Rank 0 in MPI_COMM_WORLD (init_comm) will get a list of the number of
+    // ranks per machine (indexed by machine ID). Note that rank 0 in init_comm
+    // will always be a member of group 1 in machine_delegate_comm.
+    int *rpm = NULL;
+    if (0 == inf->rank) {
+        rpm = calloc(inf->n_machines, sizeof(*rpm));
+    }
+    // Note that rank 0 in init_comm will always be a member of group 1 in
+    // machine_delegate_comm.
+    if (MPI_COMM_NULL != inf->machine_delegate_comm) {
+        MPI_Gather(&inf->nqid, 1, MPI_INT, rpm, 1, MPI_INT,
+                   0, inf->machine_delegate_comm);
+    }
+#if 1
+    if (inf->rank == 0) {
+        for (int i = 0; i < inf->n_machines; ++i) {
+            printf("rank %d says rpm on machine id %d = %d\n", 0, i, rpm[i]);
+        }
+    }
+#endif
+    //
+    int selected = 0;
+    QUO_auto_distrib(inf->quo, target_res, max_local_res, &selected);
 }
 
 static int
@@ -92,35 +106,34 @@ init(inf_t *inf)
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &(inf->nranks));
     MPI_Comm_rank(MPI_COMM_WORLD, &(inf->rank));
-
-    /* QUO Stuff //////////////////////////////////////////////////////////// */
+    //
     inf->init_comm = MPI_COMM_WORLD; /* Init QUO with MPI_COMM_WORLD. */
     inf->machine_comm = MPI_COMM_NULL;
     inf->machine_delegate_comm = MPI_COMM_NULL;
-
+    // QUO stuff
     QUO_create(&inf->quo, inf->init_comm);
     QUO_nnodes(inf->quo, &inf->n_machines);
     QUO_id(inf->quo, &inf->qid);
     QUO_nqids(inf->quo, &inf->nqid);
-
+    //
     machine_info_setup(inf);
-
+    //
     return 0;
 }
 
 static void
 fini(inf_t *inf)
 {
-    /* QUO communicator cleanup */
+    // QUO communicator cleanup.
     if (inf->machine_comm != MPI_COMM_NULL) {
         MPI_Comm_free(&inf->machine_comm);
     }
     if (inf->machine_delegate_comm != MPI_COMM_NULL) {
         MPI_Comm_free(&inf->machine_delegate_comm);
     }
-
+    //
     QUO_free(inf->quo);
-
+    //
     MPI_Finalize();
 }
 
@@ -129,21 +142,22 @@ main(void)
 {
     inf_t info;
     init(&info);
-    //
+    // Modify here for testing different configurations.
     QUO_obj_type_t target_res = QUO_OBJ_NUMANODE;
-    int max_global_res = 3;
+    int max_local_res  = 2;
+    int max_global_res = 5;
+    // Holds the results.
     int *tids = NULL, ntids = -1;
     //
-    tid_setup(&info, target_res, max_global_res, &tids, &ntids);
-
-    assert(max_global_res == ntids);
-
+    tid_setup(&info, target_res, max_local_res, max_global_res, &tids, &ntids);
+    //assert(max_global_res == ntids);
+    //
     if (0 == info.rank) {
         printf("### n_machines: %d\n", info.n_machines);
     }
-
+    // Cleanup.
     free(tids);
     fini(&info);
-
+    // All done.
     return EXIT_SUCCESS;
 }
