008fb066a6d293f49e12d728c8e552bc6bf9cd30
Samuel K. Gutierrez <samuel@lanl.gov>
2018-03-13 22:37:32 -0600

    Checkpoint

--- demos/nice-tid-distrib.c
+++ demos/nice-tid-distrib.c
@@ -11,94 +11,124 @@
 
 #include "mpi.h"
 
+// An example of how one might get a 'nice distribution' of tids per node when
+// faced with a potentially uneven number of selected tids per some given
+// resource.
+
 typedef struct inf_t {
+    // My ID in MPI_COMM_WORLD.
     int rank;
+    // Number of processors in MPI_COMM_WORLD.
     int nranks;
-    /* QUO stuff below. */
+    // Yup, a QUO context.
     QUO_context quo;
-    /* The communicator used to initialize QUO. */
-    MPI_Comm init_comm;
-    /* Machine-local ID assigned by QUO. */
+    // Machine-local ID assigned by QUO.
     int qid;
-    /* Number of MPI processes that share a node with me (includes me). */
+    // Number of MPI processes that share a node with me (includes me).
     int nqid;
-    /* Number of machines participating in initializing communicator. */
+    // Number of machines participating in initializing communicator.
     int n_machines;
-    /* My machine ID (globally unique across machines). */
+    // My machine ID (globally unique across machines).
     int machine_id;
-    /* Comm used to communicate between processes on a single machine. */
+    // Comm used to communicate between processes on a single machine.
     MPI_Comm machine_comm;
-    /* Comm used for communication between qid zeros. */
+    // Comm used for communication between QID zeros across machines.
     MPI_Comm machine_delegate_comm;
+    // List of nQID totals indexed by node ID
+    // (Valid only at MPI_COMM_WORLD rank 0).
+    int *nqids;
 } inf_t;
 
-/*
- * This is where all the machine naming, communicator setup and all that jazz
- * happens.
- */
 static void
-machine_info_setup(inf_t *inf)
+machine_comm_setup(inf_t *inf)
 {
-    /* Get a communicator that has all the processes that share a machine. */
+    // Get a communicator that has all the processes that share a machine.
     QUO_get_mpi_comm_by_type(inf->quo, QUO_OBJ_MACHINE, &inf->machine_comm);
-    /* Split QUO's init comm into two groups:
-     *     1) containing qid zeros and
-     *     2) everyone else
-     */
-    MPI_Comm_split(inf->init_comm, 0 ? 1 : inf->qid == 0 /* only two colors */,
-                   inf->rank, &inf->machine_delegate_comm);
-    /* Since we want it to be erroneous for a non-zero qid to communicate over
-     * the machine_delegate_comm, invalidate for those that aren't part of group
-     * 1 above, so errors are easy to catch---it is an error to communicate over
-     * MPI_COMM_NULL. */
+}
+
+static void
+delegate_comm_setup(inf_t *inf)
+{
+    // Split MPI_COMM_WORLD into two groups:
+    //     Group 1: contains all QID zeros in the job.
+    //     Group 2) contains everyone else.
+    MPI_Comm_split(
+        MPI_COMM_WORLD,
+        0 ? 1 : inf->qid == 0 /* Only two groups. */,
+        inf->rank,
+        &inf->machine_delegate_comm
+    );
+    // Because we want it to be erroneous for a non-zero QID to communicate over
+    // the machine_delegate_comm, invalidate for those that aren't part of Group
+    // 1 (above), so errors are easy to catch---it is an error to communicate
+    // over MPI_COMM_NULL.
     if (inf->qid != 0) {
         MPI_Comm_free(&inf->machine_delegate_comm);
         inf->machine_delegate_comm = MPI_COMM_NULL;
     }
-    /* Name the nodes: 0 to n-1 */
-    if (MPI_COMM_NULL != inf->machine_delegate_comm) {
+}
+
+static void
+node_id_setup(inf_t *inf)
+{
+    // Name the machines with IDs 0 to N-1, where N is the total number of
+    // machines in the job (because MPI_COMM_WORLD was used in QUO_create).
+    if (inf->machine_delegate_comm != MPI_COMM_NULL) {
+        // The reason this works is because each node has exactly one process in
+        // the machine_delegate_comm communicator---and they happen to be nice
+        // IDs: 0 to N-1.
         MPI_Comm_rank(inf->machine_delegate_comm, &inf->machine_id);
-        /* Make sure this is sane. */
+        // Not strictly needed---a sanity check.
         int sanity = -1;
         MPI_Comm_size(inf->machine_delegate_comm, &sanity);
         assert(sanity == inf->n_machines);
     }
-    /* Share the name with everyone on a node. */
+    // Share the name with everyone on a node.
     MPI_Bcast(&inf->machine_id, 1, MPI_INT, 0, inf->machine_comm);
 }
 
 static void
-tid_setup(inf_t *inf,
-          QUO_obj_type_t target_res,
-          int max_local_res,
-          int max_global_res,
-          int **out_tids,
-          int *out_tids_len)
-{
-    // Rank 0 in MPI_COMM_WORLD (init_comm) will get a list of the number of
-    // ranks per machine (indexed by machine ID). Note that rank 0 in init_comm
-    // will always be a member of group 1 in machine_delegate_comm.
-    int *rpm = NULL;
-    if (0 == inf->rank) {
-        rpm = calloc(inf->n_machines, sizeof(*rpm));
+gather_nqids(
+    inf_t *inf
+) {
+    if (inf->rank == 0) {
+        // Create list large enough to hold nqid totals across all machines.
+        inf->nqids = calloc(inf->n_machines, sizeof(int));
     }
-    // Note that rank 0 in init_comm will always be a member of group 1 in
-    // machine_delegate_comm.
-    if (MPI_COMM_NULL != inf->machine_delegate_comm) {
-        MPI_Gather(&inf->nqid, 1, MPI_INT, rpm, 1, MPI_INT,
-                   0, inf->machine_delegate_comm);
+    else {
+        inf->nqids = NULL;
     }
-#if 1
+    // MPI_COMM_WORLD rank 0 will always be a node delegate. We also know that
+    // the number of nodes (machines) reported by QUO will be equal to the size
+    // of machine_delegate_comm.
+    if (inf->machine_delegate_comm != MPI_COMM_NULL) {
+        MPI_Gather(
+            &inf->nqid,
+            1,
+            MPI_INT,
+            inf->nqids,
+            1,
+            MPI_INT,
+            0,
+            inf->machine_delegate_comm
+        );
+    }
     if (inf->rank == 0) {
         for (int i = 0; i < inf->n_machines; ++i) {
-            printf("rank %d says rpm on machine id %d = %d\n", 0, i, rpm[i]);
+            printf(
+                "# rank %d says that node %d has %d processes.\n",
+                inf->rank, i, inf->nqids[i]
+            );
         }
     }
-#endif
-    //
+    // Not needed. Only used for nice output.
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+#if 0
     int selected = 0;
     QUO_auto_distrib(inf->quo, target_res, max_local_res, &selected);
-}
+#endif
 
 static int
 init(inf_t *inf)
@@ -106,17 +136,14 @@ init(inf_t *inf)
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &(inf->nranks));
     MPI_Comm_rank(MPI_COMM_WORLD, &(inf->rank));
-    //
-    inf->init_comm = MPI_COMM_WORLD; /* Init QUO with MPI_COMM_WORLD. */
-    inf->machine_comm = MPI_COMM_NULL;
-    inf->machine_delegate_comm = MPI_COMM_NULL;
     // QUO stuff
-    QUO_create(&inf->quo, inf->init_comm);
-    QUO_nnodes(inf->quo, &inf->n_machines);
+    QUO_create(&inf->quo, MPI_COMM_WORLD);
     QUO_id(inf->quo, &inf->qid);
     QUO_nqids(inf->quo, &inf->nqid);
+    QUO_nnodes(inf->quo, &inf->n_machines);
     //
-    machine_info_setup(inf);
+    inf->machine_comm = MPI_COMM_NULL;
+    inf->machine_delegate_comm = MPI_COMM_NULL;
     //
     return 0;
 }
@@ -141,22 +168,21 @@ int
 main(void)
 {
     inf_t info;
+    //
     init(&info);
+    //
+    machine_comm_setup(&info);
+    //
+    delegate_comm_setup(&info);
+    //
+    node_id_setup(&info);
+    //
+    gather_nqids(&info);
     // Modify here for testing different configurations.
-    QUO_obj_type_t target_res = QUO_OBJ_NUMANODE;
-    int max_local_res  = 2;
-    int max_global_res = 5;
-    // Holds the results.
-    int *tids = NULL, ntids = -1;
-    //
-    tid_setup(&info, target_res, max_local_res, max_global_res, &tids, &ntids);
-    //assert(max_global_res == ntids);
-    //
-    if (0 == info.rank) {
-        printf("### n_machines: %d\n", info.n_machines);
-    }
+    const QUO_obj_type_t target_res = QUO_OBJ_NUMANODE;
+    const int max_local_res  = 2; // Per node.
+    const int max_global_res = 5; // Total target across all machines.
     // Cleanup.
-    free(tids);
     fini(&info);
     // All done.
     return EXIT_SUCCESS;
