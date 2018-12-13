beba1ab3fe6738f41d3f43ab47463698150e5915
Samuel K. Gutierrez <samuel@lanl.gov>
2018-12-13 15:33:42 -0700

    Fix inconstancies in quo-time.

--- tests/quo-time.c
+++ tests/quo-time.c
@@ -1,44 +1,9 @@
 /**
- * Copyright (c) 2013-2016 Los Alamos National Security, LLC
+ * Copyright (c) 2013-2018 Los Alamos National Security, LLC
  *                         All rights reserved.
  *
- * This software was produced under U.S. Government contract DE-AC52-06NA25396
- * for Los Alamos National Laboratory (LANL), which is operated by Los Alamos
- * National Security, LLC for the U.S.  Department of Energy. The U.S.
- * Government has rights to use, reproduce, and distribute this software.
- * NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY
- * WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS
- * SOFTWARE.  If software is modified to produce derivative works, such modified
- * software should be clearly marked, so as not to confuse it with the version
- * available from LANL.
- *
- * Additionally, redistribution and use in source and binary forms, with or
- * without modification, are permitted provided that the following conditions
- * are met:
- *
- * · Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * · Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * · Neither the name of Los Alamos National Security, LLC, Los Alamos
- *   National Laboratory, LANL, the U.S. Government, nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
- * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL
- * SECURITY, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This file is part of the libquo project. See the LICENSE file at the
+ * top-level directory of this distribution.
  */
 
 #include "quo.h"
@@ -53,7 +18,7 @@
 #include "mpi.h"
 
 /**
- * Measures cost of QUO calls.
+ * Measures operational latencies of QUO calls.
  */
 
 typedef struct context_t {
@@ -61,30 +26,9 @@ typedef struct context_t {
     int rank;
     /* number of ranks in MPI_COMM_WORLD */
     int nranks;
-    /* number of nodes in our job */
-    int nnodes;
-    /* number of ranks that share this node with me (includes myself) */
-    int nnoderanks;
-    /* my node rank */
-    int noderank;
     /* whether or not mpi is initialized */
     bool mpi_inited;
-    /* number of sockets on the node */
-    int nsockets;
-    /* number of cores on the node */
-    int ncores;
-    /* number of pus (processing units - e.g hw threads) */
-    int npus;
-    /* quo major version */
-    int qv;
-    /* quo minor version */
-    int qsv;
-    /* pointer to initial stringification of our cpuset */
-    char *cbindstr;
-    /* flag indicating whether or not we are initially bound */
-    int bound;
-    /* our quo context (the thing that gets passed around all over the place).
-     * filler words that make this comment line look mo better... */
+    /* QUO context used for some tests. */
     QUO_context quo;
 } context_t;
 
@@ -114,7 +58,6 @@ fini(context_t *c)
     if (QUO_SUCCESS != QUO_free(c->quo)) return 1;
     /* finalize mpi AFTER QUO_destruct - we may mpi in our destruct */
     if (c->mpi_inited) MPI_Finalize();
-    if (c->cbindstr) free(c->cbindstr);
     free(c);
     return 0;
 }
@@ -138,8 +81,6 @@ init(context_t **c)
     if (MPI_SUCCESS != MPI_Comm_size(MPI_COMM_WORLD, &(newc->nranks))) goto err;
     /* ...and more */
     if (MPI_SUCCESS != MPI_Comm_rank(MPI_COMM_WORLD, &(newc->rank))) goto err;
-    /* can be called at any point -- even before init and construct. */
-    if (QUO_SUCCESS != QUO_version(&(newc->qv), &(newc->qsv))) goto err;
     /* relatively expensive call. you only really want to do this once at the
      * beginning of time and pass the context all over the place within your
      * code. */
@@ -150,83 +91,6 @@ init(context_t **c)
 err:
     (void)fini(newc);
     return 1;
-}
-
-/**
- * gather system and job info from libquo.
- */
-static int
-sys_grok(context_t *c)
-{
-    char *bad_func = NULL;
-
-    /* this interface is more powerful, but the other n* calls can be more
-     * convenient. at any rate, this is an example of the
-     * QUO_nobjs_in_type_by_type interface to get the number of sockets on
-     * the machine. note: you can also use the QUO_nsockets or
-     * QUO_nobjs_by_type to get the same info. */
-    if (QUO_SUCCESS != QUO_nobjs_in_type_by_type(c->quo,
-                                                 QUO_OBJ_MACHINE,
-                                                 0,
-                                                 QUO_OBJ_SOCKET,
-                                                 &c->nsockets)) {
-        bad_func = "QUO_nobjs_in_type_by_type";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_ncores(c->quo, &c->ncores)) {
-        bad_func = "QUO_ncores";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_npus(c->quo, &c->npus)) {
-        bad_func = "QUO_npus";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_bound(c->quo, &c->bound)) {
-        bad_func = "QUO_bound";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_stringify_cbind(c->quo, &c->cbindstr)) {
-        bad_func = "QUO_stringify_cbind";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_nnodes(c->quo, &c->nnodes)) {
-        bad_func = "QUO_nnodes";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_nqids(c->quo, &c->nnoderanks)) {
-        bad_func = "QUO_nqids";
-        goto out;
-    }
-    if (QUO_SUCCESS != QUO_id(c->quo, &c->noderank)) {
-        bad_func = "QUO_id";
-        goto out;
-    }
-out:
-    if (bad_func) {
-        fprintf(stderr, "%s: %s failure :-(\n", __func__, bad_func);
-        return 1;
-    }
-    return 0;
-}
-
-static int
-emit_node_basics(const context_t *c)
-{
-    (void)c;
-#if 0
-    /* one proc per node will emit this info */
-    if (0 == c->noderank) {
-        printf("### %d: quo version: %d.%d ###\n", c->rank, c->qv, c->qsv);
-        printf("### %d: nnodes: %d\n", c->rank, c->nnodes);
-        printf("### %d: nnoderanks: %d\n", c->rank, c->nnoderanks);
-        printf("### %d: nsockets: %d\n", c->rank, c->nsockets);
-        printf("### %d: ncores: %d\n", c->rank, c->ncores);
-        printf("### %d: npus: %d\n", c->rank, c->npus);
-        fflush(stdout);
-    }
-    demo_emit_sync(c);
-#endif
-    return 0;
 }
 
 static int
@@ -281,20 +145,15 @@ qnpus(
     int n_trials,
     double *res
 ) {
-    //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
-    //
     int n = 0;
     for (int i = 0; i < n_trials; ++i) {
         double start = MPI_Wtime();
-        if (QUO_SUCCESS != QUO_npus(ctx, &n)) return 1;
+        if (QUO_SUCCESS != QUO_npus(c->quo, &n)) return 1;
         double end = MPI_Wtime();
         res[i] = end - start;
     }
     // Don't want compiler to optimize this away. Will never print.
     if (c->rank == (c->nranks + 1)) printf("### NPUS: %d\n", n);
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -304,17 +163,13 @@ qquids_in_type(
     int n_trials,
     double *res
 ) {
-    //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
-    //
     int n = 0;
     for (int i = 0; i < n_trials; ++i) {
         int nq = 0;
         int *qids = NULL;
         double start = MPI_Wtime();
         if (QUO_SUCCESS != QUO_qids_in_type(
-                               ctx, QUO_OBJ_PU, 0,
+                               c->quo, QUO_OBJ_PU, 0,
                                &nq, &qids)) return 1;
         double fs = MPI_Wtime();
         free(qids);
@@ -324,7 +179,6 @@ qquids_in_type(
     }
     // Don't want compiler to optimize this away. Will never print.
     if (c->rank == (c->nranks + 1)) printf("%d\n", n);
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -334,21 +188,15 @@ qbind_push(
     int n_trials,
     double *res
 ) {
-    (void)c;
-    //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
-    //
     for (int i = 0; i < n_trials; ++i) {
         double start = MPI_Wtime();
         if (QUO_SUCCESS != QUO_bind_push(
-                               ctx, QUO_BIND_PUSH_OBJ,
+                               c->quo, QUO_BIND_PUSH_OBJ,
                                QUO_OBJ_MACHINE, -1)) return 1;
         double end = MPI_Wtime();
         res[i] = end - start;
-        if (QUO_SUCCESS != QUO_bind_pop(ctx)) return 1;
+        if (QUO_SUCCESS != QUO_bind_pop(c->quo)) return 1;
     }
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -358,21 +206,16 @@ qbind_pop(
     int n_trials,
     double *res
 ) {
-    (void)c;
-    //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
     //
     for (int i = 0; i < n_trials; ++i) {
         if (QUO_SUCCESS != QUO_bind_push(
-                               ctx, QUO_BIND_PUSH_OBJ,
+                               c->quo, QUO_BIND_PUSH_OBJ,
                                QUO_OBJ_MACHINE, -1)) return 1;
         double start = MPI_Wtime();
-        if (QUO_SUCCESS != QUO_bind_pop(ctx)) return 1;
+        if (QUO_SUCCESS != QUO_bind_pop(c->quo)) return 1;
         double end = MPI_Wtime();
         res[i] = end - start;
     }
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -383,20 +226,16 @@ qauto_distrib(
     double *res
 ) {
     //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
-    //
     int sel = 0;
     for (int i = 0; i < n_trials; ++i) {
         double start = MPI_Wtime();
-        if (QUO_SUCCESS != QUO_auto_distrib(ctx, QUO_OBJ_PU,
+        if (QUO_SUCCESS != QUO_auto_distrib(c->quo, QUO_OBJ_PU,
                                             c->nranks, &sel)) return 1;
         double end = MPI_Wtime();
         res[i] = end - start;
     }
     // Don't want compiler to optimize this away. Will never print.
     if (c->rank == (c->nranks + 1)) printf("### NPUS: %d\n", sel);
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -406,18 +245,12 @@ qbarrier(
     int n_trials,
     double *res
 ) {
-    (void)c;
-    //
-    QUO_context ctx;
-    if (QUO_SUCCESS != QUO_create(&ctx, MPI_COMM_WORLD)) return 1;
-    //
     for (int i = 0; i < n_trials; ++i) {
         double start = MPI_Wtime();
-        if (QUO_SUCCESS != QUO_barrier(ctx)) return 1;
+        if (QUO_SUCCESS != QUO_barrier(c->quo)) return 1;
         double end = MPI_Wtime();
         res[i] = end - start;
     }
-    if (QUO_SUCCESS != QUO_free(ctx)) return 1;
     return 0;
 }
 
@@ -535,20 +368,7 @@ main(void)
         bad_func = "init";
         goto out;
     }
-    /* ////////////////////////////////////////////////////////////////////// */
-    /* libquo is now ready for service */
-    /* ////////////////////////////////////////////////////////////////////// */
 
-    /* first gather some info so we can base our decisions on our current
-     * situation. */
-    if (sys_grok(context)) {
-        bad_func = "sys_grok";
-        goto out;
-    }
-    if (emit_node_basics(context)) {
-        bad_func = "emit_node_basics";
-        goto out;
-    }
     if (0 == context->rank) {
         printf("### Starting QUO Timing Tests...\n");
         fflush(stdout);
